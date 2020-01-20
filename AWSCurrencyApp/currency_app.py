c789db882a2d11175fc58f2d7707f77712078857
Karam <karam.abbas@digital.hmrc.gov.uk>
2020-01-20 13:48:48 +0000

    Updated code

--- AWSCurrencyApp/currency_app.py
+++ AWSCurrencyApp/currency_app.py
@@ -12,18 +12,16 @@ class QueueProcessor:
     sqs = sqs = boto3.client('sqs', region_name='us-east-1')
     s3 = boto3.client('s3', region_name='us-east-1')
     c = CurrencyConverter()
+    seconds = time.time()
 
     def __init__(self, queue_url, bucket_name):
         self.queue_url = queue_url
         self.bucket_name = bucket_name
 
-    seconds = time.time()
-    queue_url = "https://sqs.us-east-1.amazonaws.com/117670899390/SQSQueue"
-
     def receive_message(self):
         try:
             response = self.sqs.receive_message(
-                QueueUrl=queue_url,
+                QueueUrl=self.queue_url,
                 AttributeNames=[
                     'SentTimestamp'
                 ],
@@ -46,8 +44,8 @@ class QueueProcessor:
             key_name = json_body["Records"][0]["s3"]["object"]["key"]
             print("Reading " + key_name)
 
-    def get_file():
-        file_data = s3.get_object(Bucket="inputbucketforqueue", Key=key_name)
+    def get_file(self):
+        file_data = self.s3.get_object(Bucket="inputbucketforqueue", Key=key_name)
         global csv_string
         csv_string = file_data['Body'].read().decode('utf-8')
         reader = csv.DictReader(io.StringIO(csv_string))
@@ -55,39 +53,42 @@ class QueueProcessor:
             print(row)
         print(" ")
 
-    def convert_currencies():
+    def convert_currencies(self):
         reader = csv.DictReader(io.StringIO(csv_string))
         data = list(reader)
         for index, row in enumerate(data):
             if row["Currency"] != 'GBP':
-                data[index]["Price"] = round(c.convert(float(row["Price"]), 'GBP'), 2)
+                data[index]["Price"] = round(self.c.convert(float(row["Price"]), 'GBP'), 2)
         json_file = json.dumps(data, indent=4, sort_keys=True)
         print(json_file)
-        file_key = "CSV" + str(seconds) + ".csv"
+        file_key = "CSV" + str(self.seconds) + ".csv"
         print("New filename: " + file_key)
-        s3.put_object(Body=json_file, Bucket="outputbuckerforec2", Key=file_key)        
+        self.s3.put_object(Body=json_file, Bucket="outputbuckerforec2", Key=file_key)        
 
-    def delete_message():
-        response = sqs.delete_message(
-            QueueUrl=queue_url,
+    def delete_message(self):
+        response = self.sqs.delete_message(
+            QueueUrl=self.queue_url,
             ReceiptHandle=receipt_handle
         )
         print("Deleted " + receipt_handle)
 
-    def delete_file():
-        response = s3.delete_object(
+    def delete_file(self):
+        response = self.s3.delete_object(
             Bucket="inputbucketforqueue",
             Key=key_name
         )
         print("Deleted " + key_name)
 
-    while True:
-        try:
-            receive_message(self)
-            delete_message()
-            get_file()
-            convert_currencies() 
-            delete_file()
-        except KeyError:
-            print("No messages available. Trying again in 60 secs.")
-            time.sleep(60)
+    def start(self):
+        while True:
+            try:
+                self.receive_message()
+                self.delete_message()
+                self.get_file()
+                self.convert_currencies() 
+                self.delete_file()
+            except KeyError:
+                print("No messages available. Trying again in 60 secs.")
+                time.sleep(60)
+
+my_queue = QueueProcessor("https://sqs.us-east-1.amazonaws.com/117670899390/SQSQueue", "inputbucketforqueue").start()
