cb6ae0a08f6d26fc6df97b78ec8ec044f9543f94
Karam <karam.abbas@digital.hmrc.gov.uk>
2020-01-20 10:25:59 +0000

    Updated code

--- AWSCurrencyApp/currency_app.py
+++ AWSCurrencyApp/currency_app.py
@@ -17,80 +17,77 @@ class QueueProcessor:
         self.queue_url = queue_url
         self.bucket_name = bucket_name
 
-c = CurrencyConverter()
-seconds = time.time()
-sqs = boto3.client('sqs', region_name='us-east-1')
-s3 = boto3.client('s3', region_name='us-east-1')
-queue_url = "https://sqs.us-east-1.amazonaws.com/117670899390/SQSQueue"
+    seconds = time.time()
+    queue_url = "https://sqs.us-east-1.amazonaws.com/117670899390/SQSQueue"
 
-def receive_message():
-    try:
-        response = sqs.receive_message(
+    def receive_message(self):
+        try:
+            response = self.sqs.receive_message(
+                QueueUrl=queue_url,
+                AttributeNames=[
+                    'SentTimestamp'
+                ],
+                MaxNumberOfMessages=1,
+                MessageAttributeNames=[
+                    'All'
+                ],
+                VisibilityTimeout=0,
+                WaitTimeSeconds=20
+            )
+            global receipt_handle
+            receipt_handle = response["Messages"][0]["ReceiptHandle"]
+        except KeyError:    
+            raise
+        else:
+            print("Reading " + receipt_handle)
+            body = response["Messages"][0]["Body"]
+            json_body = json.loads(body)
+            global key_name
+            key_name = json_body["Records"][0]["s3"]["object"]["key"]
+            print("Reading " + key_name)
+
+    def get_file():
+        file_data = s3.get_object(Bucket="inputbucketforqueue", Key=key_name)
+        global csv_string
+        csv_string = file_data['Body'].read().decode('utf-8')
+        reader = csv.DictReader(io.StringIO(csv_string))
+        for row in reader:
+            print(row)
+        print(" ")
+
+    def convert_currencies():
+        reader = csv.DictReader(io.StringIO(csv_string))
+        data = list(reader)
+        for index, row in enumerate(data):
+            if row["Currency"] != 'GBP':
+                data[index]["Price"] = round(c.convert(float(row["Price"]), 'GBP'), 2)
+        json_file = json.dumps(data, indent=4, sort_keys=True)
+        print(json_file)
+        file_key = "CSV" + str(seconds) + ".csv"
+        print("New filename: " + file_key)
+        s3.put_object(Body=json_file, Bucket="outputbuckerforec2", Key=file_key)        
+
+    def delete_message():
+        response = sqs.delete_message(
             QueueUrl=queue_url,
-            AttributeNames=[
-                'SentTimestamp'
-            ],
-            MaxNumberOfMessages=1,
-            MessageAttributeNames=[
-                'All'
-            ],
-            VisibilityTimeout=0,
-            WaitTimeSeconds=20
+            ReceiptHandle=receipt_handle
         )
-        global receipt_handle
-        receipt_handle = response["Messages"][0]["ReceiptHandle"]
-    except KeyError:    
-        raise
-    else:
-        print("Reading " + receipt_handle)
-        body = response["Messages"][0]["Body"]
-        json_body = json.loads(body)
-        global key_name
-        key_name = json_body["Records"][0]["s3"]["object"]["key"]
-        print("Reading " + key_name)
+        print("Deleted " + receipt_handle)
 
-def get_file():
-    file_data = s3.get_object(Bucket="inputbucketforqueue", Key=key_name)
-    global csv_string
-    csv_string = file_data['Body'].read().decode('utf-8')
-    reader = csv.DictReader(io.StringIO(csv_string))
-    for row in reader:
-        print(row)
-    print(" ")
+    def delete_file():
+        response = s3.delete_object(
+            Bucket="inputbucketforqueue",
+            Key=key_name
+        )
+        print("Deleted " + key_name)
 
-def convert_currencies():
-    reader = csv.DictReader(io.StringIO(csv_string))
-    data = list(reader)
-    for index, row in enumerate(data):
-        if row["Currency"] != 'GBP':
-            data[index]["Price"] = round(c.convert(float(row["Price"]), 'GBP'), 2)
-    json_file = json.dumps(data, indent=4, sort_keys=True)
-    print(json_file)
-    file_key = "CSV" + str(seconds) + ".csv"
-    print("New filename: " + file_key)
-    s3.put_object(Body=json_file, Bucket="outputbuckerforec2", Key=file_key)        
-
-def delete_message():
-    response = sqs.delete_message(
-        QueueUrl=queue_url,
-        ReceiptHandle=receipt_handle
-    )
-    print("Deleted " + receipt_handle)
-
-def delete_file():
-    response = s3.delete_object(
-        Bucket="inputbucketforqueue",
-        Key=key_name
-    )
-    print("Deleted " + key_name)
-
-while True:
-    try:
-        receive_message()
-        delete_message()
-        get_file()
-        convert_currencies() 
-        delete_file()
-    except KeyError:
-        print("No messages available. Trying again in 60 secs.")
-        time.sleep(60)
+    while True:
+        try:
+            receive_message(self)
+            delete_message()
+            get_file()
+            convert_currencies() 
+            delete_file()
+        except KeyError:
+            print("No messages available. Trying again in 60 secs.")
+            time.sleep(60)
